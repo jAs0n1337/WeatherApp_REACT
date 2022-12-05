75fc4ff3c1a9728e69aba828d5cd24965d1c100d
jAs0n1337 <steffen.dietrich86@gmail.com>
2022-12-05 14:55:46 +0100

    ReactWeather

---
+++ src/components/Weather.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import './Weather.css'
+import key from "../key.json"
+
+// const key = "b319a77fb8e4fe01a5b2b5cbdab2669f";
+
+function Weather() {
+    const [weatherData, setWeatherData] = useState();
+    const [country, setCountry] = useState("berlin");
+
+    console.log(weatherData);
+
+    useEffect(() => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${key.APIkey}&units=metric`)
+            .then((res) => res.json())
+            .then((data) => {
+                setWeatherData(data);
+
+            });
+    }, [country]);
+
+    if (weatherData === undefined) {
+        return;
+    }
+
+    return (
+        <div className="divWeather">
+            <div className="chooseCountry">
+                <button onClick={() => setCountry("Düsseldorf")}>Düsseldorf</button>
+                <button onClick={() => setCountry("Köln")}>Köln</button>
+                <button onClick={() => setCountry("Berlin")}>Berlin</button>
+                <button onClick={() => setCountry("Hamburg")}>Hamburg</button>
+
+            </div>
+            <div className="outputDates">
+                <p>{weatherData.name}</p>
+                <p>{weatherData.weather.description} in {weatherData.name}</p>
+                <img alt="" src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}></img>
+                <p>{weatherData.main.temp}°C</p>
+                <p>{weatherData.wind.speed} meter/sec</p>
+            </div>
+        </div >
+    );
+};
+
+export default Weather;
